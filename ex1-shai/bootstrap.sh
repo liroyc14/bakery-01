a3f639d2b5603fde6def6290e12ef5bd2fb187b9
Shai Kamenker <kamenker92@gmail.com>
2020-10-26 07:24:19 -0400

    Fixed scripts: removed sudo, changed shebang, removed comments..

--- ex1-shai/bootstrap.sh
+++ ex1-shai/bootstrap.sh
@@ -4,7 +4,7 @@
 yum update -y 
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-echo "Installing docker"
+echo "Installing docker....."
 yum install -y docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
